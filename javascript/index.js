3a1c6087204c2e4be7a0c5700d9b0054ede895ca
bridgemami <mdbridgeman@gmail.com>
2022-09-23 15:51:09 -0700

    finished exercise

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+//Using the .length property, console log the number of letters in “Supercalifragilisticexpialidocious”
+let sup = "Supercalifragilisticexpialidocious";
+console.log(`${sup} length is ` + sup.length +'.')
+
+//Console log any number
+let num = 5555555;
+let bigInt =99999n;
+
+console.log(`${num} is a ` + typeof(num)+ `. \n${bigInt} is a ` + typeof(bigInt)+ `.`);
+
